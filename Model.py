700eb4db5b3ab630e2f605673cfd036754ce9547
Aleksey Kuzin <kuzinmails@yandex.ru>
2025-06-05 17:54:01 +0300

    mock model & type specs

--- Model.py
+++ Model.py
@@ -1,5 +1,7 @@
 import torch
 import torch.nn as nn
+import onnxruntime as ort
+import json
 
 class MockModel(nn.Module):
     def __init__(self, embed_dim, num_heads, num_blocks, mock_output=False):
@@ -26,24 +28,59 @@ if __name__ == "__main__":
     n_blocks = 4
     n_heads = 3
     embed_dim = 196
-    B =1
+    B = 1
     max_seq_len = 128
     N_c = 3000
-    candidates = torch.rand(B,embed_dim, N_c) # user - candidates variant
+    candidates = torch.rand(B,embed_dim, N_c) # user <-> candidates
     x = torch.rand(B, max_seq_len, embed_dim)
     inputs = (x, candidates)
-    model =  MockModel(embed_dim, n_blocks, n_heads)
+    model = MockModel(embed_dim, n_blocks, n_heads)
     model.eval()
     #print(model(x,candidates).shape)
     torch.onnx.export(
         model,
         inputs,
-        "multi_input_model.onnx",
+        "model.onnx",
         export_params=True,
         opset_version=13,
         do_constant_folding=True,
         input_names=['x', 'candidates'],
-        output_names=['output'],
+        output_names=['logits'],
     )
+
+    onnx_session = ort.InferenceSession("model.onnx")
+    input_onnx_info  = [{
+        "name": i.name,
+        "type": i.type,
+        "shape": i.shape,
+    }for i in onnx_session.get_inputs()]
+    output_onnx_info  = [{
+        "name": i.name,
+        "type": i.type,
+        "shape": i.shape,
+    }for i in onnx_session.get_outputs()]
+
+    io_info  = {"inputs": input_onnx_info, "outputs": output_onnx_info}
+
+    with open("onnx_io_info.json", "w") as f:
+        json.dump(io_info, f)
+
+    onnx_inputs = {
+        "x": x.cpu().numpy(),
+        "candidates": candidates.cpu().detach().numpy()
+    }
+
+
+    onnx_logits_list  = onnx_session.run(None, onnx_inputs)[0]
+    onnx_logits = torch.from_numpy(onnx_logits_list).to("cpu")
+    print(onnx_logits.shape)
+
+    with open("input.json", "w") as f:
+        for obj_ in onnx_inputs:
+            onnx_inputs[obj_] = onnx_inputs[obj_].tolist()
+        json.dump(onnx_inputs, f)
+
+    with open("output.json", "w") as f:
+        json.dump({"logits": onnx_logits_list[0].tolist()}, f)
 
 
